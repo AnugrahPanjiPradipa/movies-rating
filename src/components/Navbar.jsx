e2a01c6db12f64728e1b087278153a88f21f07e1
AnugrahPanjiPradipa <panjipradipa714@gmail.com>
2025-10-18 08:41:17 +0700

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from 'react';
+import movie from '../assets/movie.png';
 
 const Navbar = () => {
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <nav className="navbar bg-dark py-3">
+      <div className="container-fluid justify-content-between">
+        <a className="navbar-brand px-3 text-light d-flex align-items-center gap-1">
+          <img
+            src={movie}
+            alt="movie icon"
+            width={48}
+            height={48}
+          />
+          <span className="fs-4 fw-semibold">MoviesRate</span>
+        </a>
+        <form
+          className="d-flex mx-auto"
+          role="search"
+          style={{ width: '50%' }}
+        >
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+        </form>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
